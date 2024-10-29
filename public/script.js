315a9806e429b3e0ba28a34bd8382e5a04959b2c
unknown <mariaelima73>
2024-10-29 09:25:46 -0300

    Dia 3

--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
 const buttonLogin = document.querySelector("#login");
 const modal = document.querySelector("dialog");
-const buttonFechar = document.getElementsByClassName("fechar");
-
+const buttonFechar = document.querySelector("#fechar");
+const buttonFechar2 = document.querySelector(".fechar");
 buttonLogin.onclick = function() {
     modal.showModal();
 }
-
 buttonFechar.onclick = function() {
+    modal.close();
+}
+buttonFechar2.onclick = function() {
     modal.close();
 }
